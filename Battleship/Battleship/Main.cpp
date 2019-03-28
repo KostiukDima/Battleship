006506a5c53fac8d2de8de5bc60098e395739957
Kostiuk Dima <1dima.kostyk@gmail.com>
2019-03-28 21:30:08 +0200

    Output

--- Battleship/Battleship/Main.cpp
+++ Battleship/Battleship/Main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<Windows.h>
 #include<ctime>
+#include <stdio.h>
 using namespace std;
 
 void Fill(int arr[][10]);
@@ -46,29 +47,113 @@ void Fill(int arr[][10])
 
 void Output(int arrOne[][10], int arrTwo[][10])
 {
+	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+	
+	cout << "\t    COMPUTER\t\t\t\tPLAYER" << endl;
+
+	cout << "   ";
+	
+	for (int i = 65; i < 75; i++)
+	{
+		cout << char(i) << "  " ;
+	}
+	cout << "     ";
+	for (int i = 65; i < 75; i++)
+	{
+		cout << char(i) << "  ";
+	}
+	cout << endl;
+	
 	for (int i = 0; i < 10; i++)
 	{
+		if (i<9)
+		{
+			cout << i+1 << "  ";
+		}
+		else
+		{
+			cout << i+1 << " ";
+		}
+		
+		
 		for (int j = 0; j < 10; j++)
 		{
 			if (arrOne[i][j] == 0)
 			{
-				cout << "*";
+				SetConsoleTextAttribute(console, 9);
+				cout << "=";
+				SetConsoleTextAttribute(console, 7);
 			}
 			else if (arrOne[i][j] == 1)
 			{
-				cout << "X";
+				SetConsoleTextAttribute(console, 9);
+				cout << "=";
+				SetConsoleTextAttribute(console, 7);
 			}
 			else if (arrOne[i][j] == 2)
 			{
+				SetConsoleTextAttribute(console, 9);
 				cout << "=";
+				SetConsoleTextAttribute(console, 7);
+			}
+			else if (arrOne[i][j] == 3)
+			{
+				SetConsoleTextAttribute(console, 7);
+				cout << "*";
+				SetConsoleTextAttribute(console, 7);
+			}
+			else if (arrOne[i][j] == 4)
+			{
+				SetConsoleTextAttribute(console, 12);
+				cout << "X";
+				SetConsoleTextAttribute(console, 7);
 			}
 
 			cout << "  ";
 		}
-		cout << "   ";
+
+		cout << "  ";
+		if (i < 9)
+		{
+			cout << i + 1 << "  ";
+		}
+		else
+		{
+			cout << i + 1 << " ";
+		}
 		for (int j = 0; j < 10; j++)
 		{
-			cout << arrOne[i][j] << "  ";
+			if (arrOne[i][j] == 0)
+			{
+				SetConsoleTextAttribute(console, 9);
+				cout << "=";
+				SetConsoleTextAttribute(console, 7);
+			}
+			else if (arrOne[i][j] == 1)
+			{
+				SetConsoleTextAttribute(console, 2);
+				cout << "X";
+				SetConsoleTextAttribute(console, 7);
+			}
+			else if (arrOne[i][j] == 2)
+			{
+				SetConsoleTextAttribute(console, 9);
+				cout << "=";
+				SetConsoleTextAttribute(console, 7);
+			}
+			else if (arrOne[i][j] == 3)
+			{
+				SetConsoleTextAttribute(console, 7);
+				cout << "*";
+				SetConsoleTextAttribute(console, 7);
+			}
+			else if (arrOne[i][j] == 4)
+			{
+				SetConsoleTextAttribute(console, 12);
+				cout << "X";
+				SetConsoleTextAttribute(console, 7);
+			}
+			cout << "  ";
 		}
 		cout << endl;
 		cout << endl;
