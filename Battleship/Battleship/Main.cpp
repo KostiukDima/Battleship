1c6d24dd3395300f64127c8548265f48745d5a40
Kostiuk Dima <1dima.kostyk@gmail.com>
2019-03-29 14:17:50 +0200

    Manually Location

--- Battleship/Battleship/Main.cpp
+++ Battleship/Battleship/Main.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 #include<Windows.h>
 #include<ctime>
-#include <stdio.h>
+#include<string>
+
 using namespace std;
 
 void Fill(int arr[][10]);
@@ -15,23 +16,111 @@ void FillVerticallyFour(int arr[][10], int Y, int X);
 void FillVerticallyThree(int arr[][10], int Y, int X);
 void FillVerticallyTwo(int arr[][10], int Y, int X);
 void FillVerticallyOne(int arr[][10], int Y, int X);
+void ComputerVsPlayer(int computer[][10], int player[][10]);
+void ManuallyLocation(int arr[][10]);
+
+
 
 int main()
 {
+	system("mode con cols=80 lines=30");
+
 	srand(unsigned(time(NULL)));
 
-	int Computer[10][10];
+	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
 
+	int computer[10][10];
+	int player[10][10];
 
+	int choise = 0;
+	bool exit = false;
 
-	Fill(Computer);
+	/*while (exit!=true)
+	{
+		SetConsoleTextAttribute(console, 2);
+		cout << "1. Computer VS Player\n" << endl;
+		SetConsoleTextAttribute(console, 3);
+		cout << "2. Player VS Player\n" << endl;
+		SetConsoleTextAttribute(console, 14);
+		cout << "3. Hall of Fame\n" << endl;
+		SetConsoleTextAttribute(console, 12);
+		cout << "0. Exit\n" << endl;
+		SetConsoleTextAttribute(console, 7);
+		cout << "Your choice   ";
+		
+		cin >> choise;
+		if (choise == 0)
+		{
+			exit = true;
+		}
+		else if(choise == 1)
+		{
+			ComputerVsPlayer(computer, player);
+		}
+		else if (choise == 2)
+		{
+			//PlayerVsPlayer
+		}
+		else if (choise == 3)
+		{
+			//HallofFame
+		}
+		else
+		{
+			SetConsoleTextAttribute(console, 12);
+			cout << "Wrong choice try again" << endl;
+			SetConsoleTextAttribute(console, 7);			
+		}
+	}*/
 
-	RandomLocation(Computer);
+	Fill(computer);
 
-	Output(Computer, Computer);
+	ManuallyLocation(computer);
+
+	
+
+	//RandomLocation(Computer);
+
+	//Output(Computer, Computer);
 
 	system("pause");
 	return 0;
+}
+
+void ComputerVsPlayer(int computer[][10], int player[][10])
+{
+	string name;
+	cout << "Enter your name" << endl;
+	cin >> name;
+	int placement;
+	
+	
+	bool exit = false;
+
+	while (exit != true)
+	{
+		cout << "Ship placement?\n1. Auto\n2. Manually" << endl;
+		cin >> placement;
+		if (placement == 1 || placement == 2)
+		{
+			exit = true;
+		}
+		else
+		{
+			cout << "Wrong choice try again" << endl;
+		}
+		
+		if (placement == 1)
+		{
+			RandomLocation(player);
+			RandomLocation(computer);
+		}
+		else if (placement == 2)
+		{
+			//ManuallyLocation(player);
+			RandomLocation(computer);
+		}		
+	}
 }
 
 void Fill(int arr[][10])
@@ -160,6 +249,205 @@ void Output(int arrOne[][10], int arrTwo[][10])
 	}
 
 
+}
+
+void ManuallyLocation(int arr[][10])
+{
+	int location = 0;
+	int Y = 0;
+	int X = 0;
+	char tmpXY[255];
+	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	cout << "   ";
+	for (int i = 65; i < 75; i++)
+	{
+		cout << char(i) << "  ";
+	}
+	cout << endl;
+
+	for (int i = 0; i < 10; i++)
+	{
+		if (i < 9)
+		{
+			cout << i + 1 << "  ";
+		}
+		else
+		{
+			cout << i + 1 << " ";
+		}
+			
+		for (int j = 0; j < 10; j++)
+		{
+			if (arr[i][j] == 0)
+			{
+				SetConsoleTextAttribute(console, 9);
+				cout << "=";
+				SetConsoleTextAttribute(console, 7);
+			}
+			else if (arr[i][j] == 1)
+			{
+				SetConsoleTextAttribute(console, 2);
+				cout << "X";
+				SetConsoleTextAttribute(console, 7);
+			}
+			else if (arr[i][j] == 2)
+			{
+				SetConsoleTextAttribute(console, 14);
+				cout << "*";
+				SetConsoleTextAttribute(console, 7);
+			}
+			cout << "  ";
+		}
+		cout <<endl;
+		cout <<endl;
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		if (i == 0)
+		{
+			cout << "Slect ship location\n0. XXXX\n1.X\n  X\n  X\n  X" << endl;
+			cin >> location;
+		}
+		if (i == 1 || i == 2)
+		{
+			cout << "Slect ship location\n0. XXX\n1.X\n  X\n  X" << endl;
+			cin >> location;
+		}
+		if (i == 3 || i == 4 || i == 5)
+		{
+			cout << "Slect ship location\n0. XX\n1.X\n  X" << endl;
+			cin >> location;
+		}
+		if (i == 6 || i == 7 || i == 8 || i == 9)
+		{
+			cout << "Slect ship location\n0. X\n1.X" << endl;
+			cin >> location;
+		}
+	
+		if (location == 0)
+		{
+			cout << "Enter the coordinates of the starting point (Example A1)  ";
+			cin >> tmpXY;
+			
+			
+			Y = (int(tmpXY[1]) - 49);			
+			X = (int(tmpXY[0]) - 65);
+
+			cout << X << Y;
+
+
+
+			if (i == 0)
+			{
+				if ((X == 7) || (X == 8) || (X == 9))
+				{
+					i--;
+					continue;
+				}
+				else
+				{
+					FillHorizontallyFour(arr, Y, X);
+				}
+			}
+			if (i == 1 || i == 2)
+			{
+				if ((X == 8) || (X == 9) || (arr[Y][X] == 2) || (arr[Y][X + 1] == 2) || (arr[Y][X + 2] == 2) || (arr[Y][X] == 1) || (arr[Y][X + 1] == 1) || (arr[Y][X + 2] == 1) || (arr[Y][X] != 0))
+					{
+						i--;
+						continue;
+				}
+				else
+				{
+					FillHorizontallyThree(arr, Y, X);
+				}
+
+			}
+			if (i == 3 || i == 4 || i == 5)
+			{
+				if ((X == 9) || (arr[Y][X] == 2) || (arr[Y][X + 1] == 2) || (arr[Y][X] == 1) || (arr[Y][X + 1] == 1) || (arr[Y][X] != 0))
+				{
+					i--;
+					continue;
+				}
+				else
+				{
+					FillHorizontallyTwo(arr, Y, X);
+				}
+			}
+			if (i == 6 || i == 7 || i == 8 || i == 9)
+			{
+				if ((arr[Y][X] == 2) || (arr[Y][X] == 1) || (arr[Y][X] != 0))
+				{
+					i--;
+					continue;
+				}
+				else
+				{
+					FillHorizontallyOne(arr, Y, X);
+				}
+			}
+		}
+			//if (location == 1)
+			//{
+			//	Y = rand() % 10;
+			//	X = rand() % 10;
+
+			//	if (i == 0)
+			//	{
+			//		if ((Y == 7) || (Y == 8) || (Y == 9))
+			//		{
+			//			i--;
+
+			//			continue;
+			//		}
+			//		else
+			//		{
+			//			FillVerticallyFour(arr, Y, X);
+			//		}
+			//	}
+			//	if (i == 1 || i == 2)
+			//	{
+			//		if ((Y == 8) || (Y == 9) || (arr[Y][X] == 2) || (arr[Y + 1][X] == 2) || (arr[Y + 2][X] == 2) || (arr[Y][X] == 1) || (arr[Y + 1][X] == 1) || (arr[Y + 2][X] == 1) || (arr[Y][X] != 0))
+			//		{
+			//			i--;
+			//			continue;
+			//		}
+			//		else
+			//		{
+			//			FillVerticallyThree(arr, Y, X);
+			//		}
+
+			//	}
+			//	if (i == 3 || i == 4 || i == 5)
+			//	{
+			//		if ((Y == 9) || (arr[Y][X] == 2) || (arr[Y + 1][X] == 2) || (arr[Y][X] == 1) || (arr[Y + 1][X] == 1) || (arr[Y][X] != 0))
+			//		{
+			//			i--;
+			//			continue;
+			//		}
+			//		else
+			//		{
+			//			FillVerticallyTwo(arr, Y, X);
+			//		}
+			//	}
+			//	if (i == 6 || i == 7 || i == 8 || i == 9)
+			//	{
+			//		if ((arr[Y][X] == 2) || (arr[Y][X] == 1) || (arr[Y][X] != 0))
+			//		{
+			//			i--;
+			//			continue;
+			//		}
+			//		else
+			//		{
+			//			FillVerticallyOne(arr, Y, X);
+			//		}
+			//	}
+			//}
+		
+	}
+	system("cls");
 }
 
 void RandomLocation(int arr[][10])
